648f0b5f035daafe0a346da2d65b8e2911a55c82
Polina Raguilo <polinar2007@mail.ru>
2021-03-02 17:07:41 +0300

    Added error component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import DetailsPage from "./Components/details-page/details-page";
+import ErrorLoading from "./Components/Error/error-loading";
 import Header from "./Components/header/header";
 import CatalogPage from "./Views/catalog-page/catalog-page";
 import FavoritePage from "./Views/favorite-page/favorite-page";
@@ -15,6 +16,7 @@ class App extends Component {
           <Route exact path="/" component={CatalogPage} />
           <Route exact path="/favorite" component={FavoritePage} />
           <Route path="/beer/:id" component={DetailsPage} />
+          <Route path="/error" component={ErrorLoading} />
         </BrowserRouter>
       </>
     );
--- src/Components/Error/error-loading.js
+++ src/Components/Error/error-loading.js
@@ -1,11 +1,30 @@
+/* eslint-disable arrow-parens */
 import React from "react";
+import PropTypes from "prop-types";
+import "./error-loading.css";
+import { connect } from "react-redux";
 
-const ErrorLoading = () => {
+const ErrorLoading = (props) => {
   return (
-    <div>
-      <h3>ERROR!!!!!</h3>
+    <div className="error__wrapper">
+      <div className="icon__wrapper">
+        <i className="fas fa-times" />
+      </div>
+      <div className="text__wrapper">
+        <p>Something went wrong...</p>
+        <p>{props.errorMessage}</p>
+      </div>
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    errorMessage: state.beerReducer.errorMsg,
+  };
+};
 
-export default ErrorLoading;
+ErrorLoading.propTypes = {
+  errorMessage: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(ErrorLoading);
