bf9254926fdb48f1e4ab6aad42deda38a866271f
Polina Raguilo <polinar2007@mail.ru>
2021-03-03 20:07:07 +0300

    added pagination to favorite page

--- src/Components/favorite-list-items/favorite-list-items.js
+++ src/Components/favorite-list-items/favorite-list-items.js
@@ -6,16 +6,31 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./favorite-list-items.css";
 import { connect } from "react-redux";
+import { Pagination } from "@material-ui/lab";
 import FavoriteItem from "./favorite-item/favorite-item";
 import { deleteFavorite } from "../../Redux/actions/favoriteAction";
 
 class FavoriteListItems extends Component {
+  state = {
+    currPage: 1,
+  };
+
   deleteFavorite = (id) => {
     this.props.deleteFromFavorite(id);
   };
 
+  onChangePage = (page) => {
+    this.setState({ currPage: page });
+  };
+
   render() {
-    const favoriteItems = this.props.favoriteBeers.map((item) => {
+    const indexOfLastPost = this.state.currPage * 5;
+    const indexOfFirstPost = indexOfLastPost - 5;
+    const currentItems = this.props.favoriteBeers.slice(
+      indexOfFirstPost,
+      indexOfLastPost
+    );
+    const favoriteItems = currentItems.map((item) => {
       const { id, name, tagline, description, img } = item;
       return (
         <FavoriteItem
@@ -33,6 +48,16 @@ class FavoriteListItems extends Component {
       <div className="favorite__wrapper">
         <h2 className="favorite__title">Your favorite beer</h2>
         {favoriteItems}
+        <Pagination
+          count={Math.ceil(this.props.favoriteBeers.length / 5)}
+          color="primary"
+          onChange={() => this.onChangePage(this.page)}
+          className={
+            this.props.favoriteBeers.length <= 5
+              ? "pagination__none"
+              : "pagination__show"
+          }
+        />
       </div>
     );
   }
