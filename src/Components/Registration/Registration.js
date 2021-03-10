e5f75bdead9efe1b0aa5ff121356d2dedbbbc950
Polina Raguilo <polinar2007@mail.ru>
2021-03-10 18:18:21 +0300

    Added registration with local storage

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,29 +1,66 @@
+/* eslint-disable arrow-parens */
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Registration.css";
 
 class RegistrationPage extends Component {
+  state = {
+    login: null,
+    password: null,
+    confirmPassword: null,
+    err: false,
+  };
+
+  onInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onRegistration = (e) => {
+    e.preventDefault();
+    const { login, password, confirmPassword } = this.state;
+    if (
+      password === confirmPassword &&
+      login &&
+      localStorage.getItem("user") !== login
+    ) {
+      this.setState({ err: false });
+      localStorage.setItem("user", login);
+    } else {
+      this.setState({ err: true });
+    }
+  };
+
   render() {
     return (
       <div className="form__wrapper">
         <h3 className="form__title">Sign Up</h3>
-        <form action="submit">
+        <h2 className={this.state.err ? "show" : "hide"}>
+          Check your data and try again
+        </h2>
+        <form action="submit" onSubmit={this.onRegistration}>
           <input
             type="text"
-            name="newLogin"
+            name="login"
             className="form__input"
             placeholder="Login"
+            onChange={this.onInputChange}
           />
           <input
             type="password"
-            name="newPassword"
+            name="password"
             className="form__input"
             placeholder="Password"
+            onChange={this.onInputChange}
           />
           <input
             type="password"
-            name="copyNewPassword"
+            name="confirmPassword"
             className="form__input"
             placeholder="Confirm password"
+            onChange={this.onInputChange}
           />
           <button type="submit" className="form__button">
             Sign Up
