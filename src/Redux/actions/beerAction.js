056ce8940c670e5e758abf5a7b4a9f7004ca278b
Polina <polinar2007@mail.ru>
2021-03-11 17:22:36 +0300

    Changes in beer Action

--- src/Redux/actions/beerAction.js
+++ src/Redux/actions/beerAction.js
@@ -27,21 +27,19 @@ const pagesOfBeer = [];
 
 export const fetchBeers = (page) => async (dispatch) => {
   dispatch(requestBeer());
-
+  let pageToLoad;
+  const isInclude = pagesOfBeer.includes(page);
+  if (isInclude === false) {
+    pagesOfBeer.push(page);
+    pageToLoad = page;
+  } else {
+    const lastPage = pagesOfBeer[Number(pagesOfBeer.length - 1)];
+    pagesOfBeer.push(lastPage + 1);
+    pageToLoad = pagesOfBeer[Number(pagesOfBeer.length - 1)];
+  }
   try {
-    const isInclude = pagesOfBeer.includes(page);
-    if (isInclude === false) {
-      pagesOfBeer.push(page);
-      const response = await beerApi.getWithPagination(page);
-      dispatch(receiveBeer(Object.values(response.data)));
-    } else {
-      const lastPage = pagesOfBeer[Number(pagesOfBeer.length - 1)];
-      pagesOfBeer.push(lastPage + 1);
-      const response = await beerApi.getWithPagination(
-        pagesOfBeer[Number(pagesOfBeer.length - 1)]
-      );
-      dispatch(receiveBeer(Object.values(response.data)));
-    }
+    const response = await beerApi.getWithPagination(pageToLoad);
+    dispatch(receiveBeer(Object.values(response.data)));
   } catch (error) {
     dispatch(failLoadBeer(error.message));
   }
